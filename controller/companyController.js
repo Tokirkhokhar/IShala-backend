c8e5249d4cbb4804f532ec8da98ca5557742eeeb
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2023-05-16 20:24:28 +0530

    login and internship add fixed

--- controller/companyController.js
+++ controller/companyController.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose")
-const asyncHandler = require("express-async-handler")
-const Company = require("../model/companyModel")
-const JWT = require("jsonwebtoken")
-const bcpt = require("bcryptjs")
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+const Company = require("../model/companyModel");
+const JWT = require("jsonwebtoken");
+const bcpt = require("bcryptjs");
 
-const signupEmployee = asyncHandler(async(req,res)=>{
-     const user = await Company.create(req.body)
-     res.status(200).json(user)
-
-})
+const signupEmployee = asyncHandler(async (req, res) => {
+  const user = await Company.create(req.body);
+  res.status(200).json(user);
+});
 
 // const getEmployeeDetails = asyncHandler(async(req,res)=>{
 //     const user = await Company.create(req.body)
@@ -16,33 +15,25 @@ const signupEmployee = asyncHandler(async(req,res)=>{
 //     console.log(user)
 // })
 
-const loginEmployee = asyncHandler(async(req,res)=>{
-    //    const user = await Company.findOne({Email, Password})  
-      const {Email,Password} = req.body
-       if(!Email || !Password){
-        res.send(400)
-        throw new Error("Please enter the correct email and password")
-       }
-   //l
+const loginEmployee = asyncHandler(async (req, res) => {
+  //    const user = await Company.findOne({Email, Password})
+  const { Email, Password } = req.body;
 
-       const user = await Company.findOne({Email})
-       if(user && await bcpt.compare(req.body.Password,user.Password)){
-        res.status(200).json({...user._doc,token : user.genToken()})
-       }
-       
+  if (!Email || !Password) {
+    res.send(400);
+    throw new Error("Please enter the correct email and password");
+  }
+  //l
 
-       else{
-        res.status(400).json("Invalid details")
-
-       }
-
-})
-
+  const user = await Company.findOne({ Email });
+  if (user && (await bcpt.compare(req.body.Password, user.Password))) {
+    res.status(200).json({ ...user._doc, token: user.genToken() });
+  } else {
+    res.status(400).json("Invalid details");
+  }
+});
 
 module.exports = {
-    signupEmployee,
-    loginEmployee,
-  
-}
-
-
+  signupEmployee,
+  loginEmployee,
+};
